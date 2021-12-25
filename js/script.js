6080111f12ee12094fcc6627f07ddad61ad0ac70
Egor Romanoff <egoromanoff@mail.ru>
2021-12-25 11:13:52 +0300

    25.12.21 added section "Blog"

--- js/script.js
+++ js/script.js
@@ -10,3 +10,16 @@ const testimonialsSlider = new Swiper('.testimonials__slider', {
     bulletActiveClass: 'dot--active',
   }
 })
+
+const blogSlider = new Swiper('.blog__slider', {
+  slidesPerView: 'auto',
+  loop: false,
+  centeredSlides: true,
+
+  pagination: {
+    el: '.slider__dots',
+    clickable: true,
+    bulletClass: 'dot',
+    bulletActiveClass: 'dot--active',
+  }
+})
