517aad593f401e92914e778b2b94e71f6432bb2c
Egor Romanoff <egoromanoff@mail.ru>
2022-01-14 01:26:09 +0300

    start editing responsivity

--- js/script.js
+++ js/script.js
@@ -26,6 +26,9 @@ const blogSlider = new Swiper('.blog__slider', {
 
 const formInputs = document.querySelectorAll('.form__input')
 const submitBtn = document.querySelector('#form__button')
+const burgerBtn = document.querySelector('#button--burger')
+const headerMenu = document.querySelector('.header__menu')
+const dimmer = document.querySelector('.dimmer')
 
 function checkInput () {
 
@@ -36,5 +39,30 @@ function checkInput () {
       submitBtn.removeAttribute('disabled')
 
 }
+
+burgerBtn.addEventListener('click', function() {
+  burgerBtn.classList.toggle('active')
+
+  if (!dimmer.classList.contains('showed')) {
+    dimmer.style.display = 'block'
+    headerMenu.classList.toggle('showed')
+    setTimeout(() => {
+      dimmer.classList.toggle('showed')
+    }, 0)
+  } else {
+    headerMenu.classList.toggle('showed')
+    setTimeout(() => {
+      dimmer.style.display = 'none'
+    }, 400)
+    dimmer.classList.toggle('showed')
+  }
+
+  if(!burgerBtn.classList.contains('active')) {
+    burgerBtn.classList.toggle('not-active')
+  } else {
+    burgerBtn.classList.remove('not-active')
+  }
+
+})
 
 formInputs.forEach(input => input.addEventListener('input', checkInput))
